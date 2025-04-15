232e3b07a01620287df7262a4ca65d762cd3a203
Samira <samira73talanchi@gmail.com>
2025-04-15 14:43:48 +0330

    add using store

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+
 function App() {
-  return <div>App</div>
+  return (
+    <Provider store={store}>
+      <div>App</div>
+    </Provider>
+  )
 }
+
+
 export default App
