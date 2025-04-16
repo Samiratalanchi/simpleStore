d2b4d39571cfc9edbf7320ec41240c39be87fbea
Samira <samira73talanchi@gmail.com>
2025-04-16 13:07:46 +0330

    add displaying product in cart

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,8 +1,35 @@
+import { useCartSelector } from "../store/hooks"
 
 function CartItems() {
+
+  const cartItems = useCartSelector((state) => state.cart.items)
+
+  const cartTotal = cartItems.reduce(
+    (value,item) => value + item.price * item.quantity , 0
+  )
+
   return (
     <div>
       <p>محصولی در سبد خرید وجود ندارد.</p>
+      <ul>
+        {cartItems.map((item) => {
+          return (
+            <div key={item.id}>
+              <div>
+                <span>{item.title}</span>
+              </div>
+              <div>
+                <button>-</button>
+                <span>{item.quantity}</span>
+                <button>+</button>
+              </div>
+            </div>
+          )
+        })}
+      </ul>
+      <div>
+        <span>مجموع:{cartTotal}تومان</span>
+      </div>
     </div>
   )
 }
